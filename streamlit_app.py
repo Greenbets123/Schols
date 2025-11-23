64fe205c23601cd2f61a2708b33bbe57ed1357e9
Greenbets <websterelliot464@gmail.com>
2025-11-23 15:48:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,7 +124,7 @@ class BlackScholes:
 with st.sidebar:
     st.title("📊 Black-Scholes Model")
     st.write("`Created by:`")
-    linkedin_url = "https://www.linkedin.com/in/elliot-webster-185564385/"
+    linkedin_url = "https://www.linkedin.com/in/elliot-webster123"
     st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Elliot Webster`</a>', unsafe_allow_html=True)
 
 
